c39cdc32fc90b6eabc2d3ea672359bfb91156163
Mateusz Żak <matzakoo@gmail.com>
2023-10-19 10:49:02 +0200

    init

---
+++ src/test/java/up/krakow/pchysioterapist/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package up.krakow.pchysioterapist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
