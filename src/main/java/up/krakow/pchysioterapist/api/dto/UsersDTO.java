63f7adc5877ae674c8ee86c1013377d3caad428a
Mateusz Żak <matzakoo@gmail.com>
2023-10-20 12:58:04 +0200

    regex dla rejestracji

--- src/main/java/up/krakow/pchysioterapist/api/dto/UsersDTO.java
+++ src/main/java/up/krakow/pchysioterapist/api/dto/UsersDTO.java
@@ -1,6 +1,13 @@
 package up.krakow.pchysioterapist.api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import up.krakow.pchysioterapist.api.validator.ValidatorUtils;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -8,7 +15,13 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class UsersDTO {
+    @NotNull
+    @Email
+    @Min(5)
     private String email;
+    @Size(min = 3, max = 20)
     private String username;
+    @Size(min = 8, max = 31)
+    @Pattern(regexp = ValidatorUtils.NO_WHITE_SPACE, message = "Nie może zawierać białych znaków")
     private String password;
 }
