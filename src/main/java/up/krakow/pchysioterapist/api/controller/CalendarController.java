8ae65a86790423b3844f563dee427ab294bf0c59
Mateusz Żak <matizakoo@gmail.com>
2023-10-30 20:44:48 +0100

    calendar + documentation

--- src/main/java/up/krakow/pchysioterapist/api/controller/CalendarController.java
+++ src/main/java/up/krakow/pchysioterapist/api/controller/CalendarController.java
@@ -14,11 +14,13 @@ import up.krakow.pchysioterapist.api.mapper.UsersMapper;
 import up.krakow.pchysioterapist.api.model.Appointment;
 import up.krakow.pchysioterapist.api.repository.AppointmentRepository;
 import up.krakow.pchysioterapist.api.repository.UsersRepository;
+import up.krakow.pchysioterapist.api.service.AppointmentServiceImpl;
 import up.krakow.pchysioterapist.api.utils.ControllerEndpoints;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = ControllerEndpoints.MOD + ControllerEndpoints.CALENDAR)
@@ -28,51 +30,10 @@ public class CalendarController {
     private final MassageMapper massageMapper;
     private final UsersMapper usersMapper;
     private final AppointmentMapper appointmentMapper;
-    private final UsersRepository usersRepository;
-    @GetMapping("/{date}")
-    public ResponseEntity<List<CalendarDTO>> getWeeklyCalendar(@PathVariable String date) {
-        System.out.println(date);
-        LocalDate localDate = LocalDate.parse(date, DateTimeFormatter.ISO_DATE);
-        System.out.println(localDate);
-        List<Appointment> appointmentList = appointmentRepository.findByStartDateBetweenOrderByStartDateAsc(localDate, localDate.plusDays(7));
-//        List<Appointment> appointmentList = appointmentRepository.findAll();
-        System.out.println(appointmentList);
-
-        Map<LocalDate, List<UsersDTO>> map = new HashMap<>();
-
-        List<AppointmentDTO> appointmentDTOList = new ArrayList<>();
-
-        for(Appointment e: appointmentList) {
-            AppointmentDTO appointmentDto = appointmentMapper.mapToAppointmentDTO(e);
-            appointmentDTOList.add(appointmentDto);
-        }
-        System.out.println(appointmentDTOList);
-
-        for(Appointment e: appointmentList) {
-            System.out.println(e.getUsers().getEmail());
-            UsersDTO usersDTO = usersMapper.mapUsersToUsersDTO(e.getUsers());
-            usersDTO.setLocalTime(e.getStartTime());
-            usersDTO.setMassageDTO(massageMapper.massageToMassageDTO(e.getMassage()));
-            if(map.containsKey(e.getStartDate())) {
-                map.get(e.getStartDate()).add(usersDTO);
-            } else {
-                map.put(e.getStartDate(), new ArrayList<>(List.of(usersDTO)));
-            }
-        }
-
-        CalendarDTO calendarDTO = new CalendarDTO();
-        List<CalendarDTO> calendarDTOList = new ArrayList<>();
-        for(LocalDate e: map.keySet()) {
-            System.out.println("MAP: " +e);
-
-            List<UsersDTO> usersDTOS = map.get(e);
-            for(UsersDTO users: usersDTOS) {
-                System.out.println(usersDTOS);
-            }
-            calendarDTOList.add(new CalendarDTO(e, map.get(e)));
-        }
-
-        return ResponseEntity.ok(calendarDTOList);
+    private final AppointmentServiceImpl appointmentService;
+    @GetMapping("/{monday}")
+    public ResponseEntity<List<CalendarDTO>> getWeeklyCalendar(@PathVariable String monday) {
+        return ResponseEntity.ok(appointmentService.getWeeklycalendar(LocalDate.parse(monday, DateTimeFormatter.ISO_DATE)));
     }
 
     @GetMapping("/e/{date}")
