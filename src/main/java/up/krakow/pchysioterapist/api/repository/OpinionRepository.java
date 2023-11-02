dbc6ec8977d089afd42527859d820e153ae75222
Mateusz Żak <matizakoo@gmail.com>
2023-11-02 09:53:01 +0100

    paginacja dla 5 opini desc

--- src/main/java/up/krakow/pchysioterapist/api/repository/OpinionRepository.java
+++ src/main/java/up/krakow/pchysioterapist/api/repository/OpinionRepository.java
@@ -1,9 +1,13 @@
 package up.krakow.pchysioterapist.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import up.krakow.pchysioterapist.api.model.Opinion;
 
 @Repository
 public interface OpinionRepository extends JpaRepository<Opinion, Integer> {
+    Page<Opinion> findAll(Pageable pageable);
+
 }
