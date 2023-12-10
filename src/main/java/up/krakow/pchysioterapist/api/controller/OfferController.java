82ef3f2d62837ab90480ed5d8e1b71a2f361b0b0
irminasciezor <irminasciezor@gmail.com>
2023-12-10 18:43:51 +0100

    podzielenie endpointów offer dla GUEST i MOD, rozbicie na dwa kontrolery

--- src/main/java/up/krakow/pchysioterapist/api/controller/OfferController.java
+++ src/main/java/up/krakow/pchysioterapist/api/controller/OfferController.java
@@ -3,6 +3,7 @@ package up.krakow.pchysioterapist.api.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import up.krakow.pchysioterapist.api.dto.InfoDTO;
 import up.krakow.pchysioterapist.api.dto.OfferDTO;
 import up.krakow.pchysioterapist.api.mapper.OfferMapper;
 import up.krakow.pchysioterapist.api.model.enums.EAppointmentType;
@@ -21,25 +22,9 @@ public class OfferController {
 
     private OfferMapper offerMapper;
 
-    @PostMapping
-    ResponseEntity<OfferDTO> createOffer(@RequestBody OfferDTO dto) {
-        return ResponseEntity.ok(offerMapper.mapToOfferDTO(offerService.createOffer(dto)));
-    }
-
     @GetMapping("/{offerId}")
     ResponseEntity<OfferDTO> getOffer(@PathVariable Integer offerId) {
         return ResponseEntity.ok(offerMapper.mapToOfferDTO(offerService.getOfferById(offerId)));
-    }
-
-    @PutMapping("/{offerId}")
-    ResponseEntity<OfferDTO> editOffer(@RequestBody OfferDTO dto, @PathVariable Integer offerId) {
-        return ResponseEntity.ok(offerMapper.mapToOfferDTO(offerService.editOffer(dto, offerId)));
-    }
-
-    @DeleteMapping("/{offerId}")
-    void deleteOffer(@PathVariable Integer offerId) {
-        //TODO: Create MessageResponse
-        offerService.deleteOffer(offerId);
     }
 
     @GetMapping("/all")
